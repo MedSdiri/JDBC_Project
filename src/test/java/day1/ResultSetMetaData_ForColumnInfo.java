8ff7f829aa2fdb5a2cb564630abb59446b924b55
Akbar <akbar@cybertekschool.com>
2021-03-09 14:39:23 -0500

    ResultSetMetaData starter

---
+++ src/test/java/day1/ResultSetMetaData_ForColumnInfo.java
@@ -0,0 +1,20 @@
+package day1;
+
+import java.sql.*;
+
+public class ResultSetMetaData_ForColumnInfo {
+
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE" ;
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection con = DriverManager.getConnection(url, username, password) ;
+        Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet  rs  = stm.executeQuery("SELECT * FROM REGIONS") ;
+
+
+    }
+
+}
