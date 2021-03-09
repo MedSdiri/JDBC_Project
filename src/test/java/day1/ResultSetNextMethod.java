6f21d9fd0f175534ab796a871fb87328e2acdf99
Akbar <akbar@cybertekschool.com>
2021-03-09 14:29:09 -0500

    going forward and backward with next and previous

--- src/test/java/day1/ResultSetNextMethod.java
+++ src/test/java/day1/ResultSetNextMethod.java
@@ -14,6 +14,7 @@ public class ResultSetNextMethod {
         Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
         ResultSet  rs  = stm.executeQuery("SELECT * FROM REGIONS") ;
 
+//        rs.beforeFirst();
         while(rs.next()){
 
             //System.out.println(rs.next() ); // DO NOT DO THIS , IT WILL MOVE YOUR CURSOR
@@ -24,6 +25,13 @@ public class ResultSetNextMethod {
             // 1. using column name  for ResultSet object's  getString(column name here)
             // 2. using column index for ResultSet object's  getString(column index here)
                     // SQL INDEX START WITH 1 !!!!!!!
+
+
+        // How do you loop backward from last row till first row
+//        rs.afterLast();
+        while( rs.previous() ){
+            System.out.println("Backward rs.getString(1) = " + rs.getString(1));
+        }
 
 
 
