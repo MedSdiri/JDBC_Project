82c4b60c7a44bef9fa76003e1f60a53e69c007ff
Akbar <akbar@cybertekschool.com>
2021-03-09 12:47:44 -0500

    Moving resultSet starter

---
+++ src/test/java/day1/MovingResultSetCursor.java
@@ -0,0 +1,21 @@
+package day1;
+
+import java.sql.*;
+
+public class MovingResultSetCursor {
+
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:oracle:thin:@54.236.150.168:1521:XE" ;
+        String username = "hr" ;
+        String password = "hr" ;
+
+        Connection con = DriverManager.getConnection(url, username, password) ;
+        Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+        ResultSet  rs  = stm.executeQuery("SELECT * FROM REGIONS") ;
+
+
+
+    }
+
+}
