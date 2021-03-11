d37d12eefffe3cfdc7edcf214d3d8b5b23175811
Akbar <akbar@cybertekschool.com>
2021-03-11 10:49:48 -0500

    Day 2 : Testing overloaded create connection method

--- src/test/java/day2/UtilityRun.java
+++ src/test/java/day2/UtilityRun.java
@@ -1,5 +1,6 @@
 package day2 ;
 
+import utility.ConfigurationReader;
 import utility.DB_Utility;
 
 public class UtilityRun {
@@ -7,6 +8,16 @@ public class UtilityRun {
     public static void main(String[] args) {
 
        DB_Utility.createConnection() ;
+
+
+       String url      = ConfigurationReader.getProperty("bookit.database.url") ;
+        String username = ConfigurationReader.getProperty("bookit.database.username") ;
+        String password = ConfigurationReader.getProperty("bookit.database.password") ;
+
+
+        DB_Utility.createConnection(url, username, password);
+
+
 
 
     }
