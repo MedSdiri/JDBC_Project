de0e86dddd1d8e7f27d0baa4f956a68707288dfa
Akbar <akbar@cybertekschool.com>
2021-03-11 11:23:09 -0500

    Day 2 : runQuery method BEING TESTED

--- src/test/java/day2/UtilityRun.java
+++ src/test/java/day2/UtilityRun.java
@@ -3,22 +3,25 @@ package day2 ;
 import utility.ConfigurationReader;
 import utility.DB_Utility;
 
+import java.sql.*;
+
 public class UtilityRun {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-       DB_Utility.createConnection() ;
+//       DB_Utility.createConnection() ;
 
 
-       String url      = ConfigurationReader.getProperty("bookit.database.url") ;
-        String username = ConfigurationReader.getProperty("bookit.database.username") ;
-        String password = ConfigurationReader.getProperty("bookit.database.password") ;
+       String url      = ConfigurationReader.getProperty("hr.database.url") ;
+        String username = ConfigurationReader.getProperty("hr.database.username") ;
+        String password = ConfigurationReader.getProperty("hr.database.password") ;
 
 
         DB_Utility.createConnection(url, username, password);
 
-
-
+        ResultSet result = DB_Utility.runQuery("SELECT * FROM REGIONS") ;
+        result.next();
+        System.out.println("region name " + result.getString("REGION_NAME"));
 
     }
 
