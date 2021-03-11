7f182c47ee486f51f6027dc5649726606451cf05
Akbar <akbar@cybertekschool.com>
2021-03-11 11:18:56 -0500

    Day 2 : runQuery method has been completed , static fields created

--- src/test/java/utility/DB_Utility.java
+++ src/test/java/utility/DB_Utility.java
@@ -4,6 +4,10 @@ import java.sql.*;
 
 public class DB_Utility {
 
+    private static Connection con ;
+    private static Statement stm ;
+    private static ResultSet rs ;
+
     /**
      * Create connection method , just checking one connection successful or not
      */
@@ -14,7 +18,7 @@ public class DB_Utility {
         String password = ConfigurationReader.getProperty("hr.database.password") ;
 
         try {
-            Connection con = DriverManager.getConnection(url , username, password) ;
+            con = DriverManager.getConnection(url , username, password) ;
             System.out.println("CONNECTION SUCCESSFUL");
         } catch (SQLException e) {
             System.out.println("CONNECTION HAS FAILED " + e.getMessage() );
@@ -33,13 +37,29 @@ public class DB_Utility {
 
 
         try {
-            Connection con = DriverManager.getConnection(url, username, password) ;
+            con = DriverManager.getConnection(url, username, password) ;
             System.out.println("CONNECTION SUCCESSFUL");
         } catch (SQLException e) {
             System.out.println("CONNECTION HAS FAILED " + e.getMessage() );
         }
 
     }
+
+
+    public static ResultSet runQuery(String sql){
+
+        try {
+             stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+             rs = stm.executeQuery(sql);
+        }catch(SQLException e){
+            System.out.println("ERROR OCCURRED WHILE RUNNING QUERY "+ e.getMessage() );
+        }
+
+         return rs ;
+
+    }
+
+
 
 
 }
