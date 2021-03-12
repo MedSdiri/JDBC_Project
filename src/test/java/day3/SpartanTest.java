4b2c0adcd26662bf9f61c51ec584b6d0c94f80fe
Akbar <akbar@cybertekschool.com>
2021-03-12 12:28:44 -0500

    Day 3 : Added Spartan DB Connection Test

--- src/test/java/day3/SpartanTest.java
+++ src/test/java/day3/SpartanTest.java
@@ -2,6 +2,7 @@ package day3;
 
 import org.junit.Test;
 import utility.ConfigurationReader;
+import utility.DB_Utility;
 
 public class SpartanTest {
 
@@ -13,7 +14,12 @@ public class SpartanTest {
         String username = ConfigurationReader.getProperty("spartan.database.username");
         String password = ConfigurationReader.getProperty("spartan.database.password");
 
+        DB_Utility.createConnection(url,username, password);
 
+        DB_Utility.runQuery("SELECT * FROM SPARTANS") ;
+        DB_Utility.displayAllData();
+
+        DB_Utility.destroy();
 
     }
 
