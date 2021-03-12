b7e760d5fd789c80130bd0bd98a2af8dd248c59a
Akbar <akbar@cybertekschool.com>
2021-03-12 10:33:54 -0500

    Day 3 : Simple DB_UI test simulaiton

---
+++ src/test/java/day3/SimpleTest.java
@@ -0,0 +1,33 @@
+package day3;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import utility.ConfigurationReader;
+import utility.DB_Utility;
+
+public class SimpleTest {
+
+    /**
+     * Assuming that we captured the UI dashboard numbers from library1 app
+     * Now we want to makes sure those numbers match the database data
+     */
+    @Test
+    public void testDashboardNumbers(){
+
+        int userCountUI = 8553 ;
+        int bookCountUI = 1992 ;
+        int borrowedBookCountUI = 800 ;
+
+        String url      = ConfigurationReader.getProperty("library1.database.url");
+        String username = ConfigurationReader.getProperty("library1.database.username");
+        String password = ConfigurationReader.getProperty("library1.database.password");
+
+        DB_Utility.createConnection(url, username, password);
+
+
+
+
+    }
+
+
+}
